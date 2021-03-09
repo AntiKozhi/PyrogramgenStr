6a375b1b616dcf32e82314025ece195bd3596df8
Anti Kozhi Official <79022061+AntiKozhi@users.noreply.github.com>
2021-03-09 11:09:33 +0530

    Create genStrSession.py

---
+++ genStrSession.py
@@ -0,0 +1,23 @@
+import os
+import asyncio
+
+from pyrogram import Client
+from dotenv import load_dotenv
+
+if os.path.isfile("config.env"):
+    load_dotenv("config.env")
+
+
+async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
+    async with Client(
+            "antikozhi",
+            api_id=int(os.environ.get("APP_ID") or input("Enter Telegram APP ID: ")),
+            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: ")
+    ) as antikozhi:
+        print("\nprocessing...")
+        await antikozhi.send_message(
+            "me", f"#AntiKozhi User Session\n\n```{await antikozhi.export_session_string()}```\n\nKeep this Safe")
+        print("Done !, Check your Saved Message!")
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(genStrSession())
